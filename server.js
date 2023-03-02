942b6d07a60974ed6cb01c21beb96b081711a068
sexozavr <daniildiveev@mail.ru>
2023-03-02 17:33:03 +0300

    [MODIFY] add post request and get requests on login

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.static('front-end'))
+app.use(express.json())
 
 const PORT = 3000;  
 
@@ -12,6 +13,14 @@ app.get('/', function(req, res) {
 
 app.get('/characteristics', function (req, res) {
     res.sendFile(path.join(__dirname, 'front-end/SecondPage.html'))
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname, 'front-end/LoginPage.html'))
+})
+
+app.post('/login', function (req, res) {
+    console.log(req.body)
 })
 
 app.listen(PORT)
