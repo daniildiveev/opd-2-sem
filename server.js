dc3d65a26f652f9b6219c62d25fec0ad08292dc6
sexozavr <daniildiveev@mail.ru>
2023-03-02 18:52:38 +0300

    [MODIFY] add user creation and change url in server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const bcrypt = require('bcrypt')
 
 app.use(express.static('front-end'))
 app.use(express.json())
 
 const PORT = 3000;  
+
+users = []
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'front-end/MenuPageDraft.html'))
@@ -19,8 +22,15 @@ app.get('/login', function (req, res) {
     res.sendFile(path.join(__dirname, 'front-end/LoginPage.html'))
 })
 
-app.post('/login', function (req, res) {
-    console.log(req.body)
+app.post('/login/register', async function (req, res) {
+    username = req.body["login"]
+    password = req.body["password"]
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(hashedPassword)
+    const user = {user:username, password:hashedPassword}
+    users.push(user)
+
 })
 
 app.listen(PORT)
