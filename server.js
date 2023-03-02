b7cd606343134c0b22003c1d3156e77978e5f294
sexozavr <daniildiveev@mail.ru>
2023-03-02 11:27:30 +0300

    [ADD] add dummy server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('front-end'))
+
+const PORT = 3000;  
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'front-end/MenuPageDraft.html'))
+})
+
+app.get('/characteristics', function (req, res) {
+    res.sendFile(path.join(__dirname, 'front-end/SecondPage.html'))
+})
+
+app.listen(PORT)
