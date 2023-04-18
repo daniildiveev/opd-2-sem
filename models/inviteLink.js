0bd549c0554354185a6af2faabeff9b70a4b0ac7
sexozavr <daniildiveev@mail.ru>
2023-04-19 00:30:53 +0300

    [MODIFY] implement logic for tests pipeline

--- models/inviteLink.js
+++ models/inviteLink.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 module.exports = function (sequelize){
     const InviteLink = sequelize.define('invite_link', {
             userWhoCreated: Sequelize.INTEGER,
-            testType: Sequelize.STRING,
+            tests: Sequelize.ARRAY(Sequelize.STRING),
             code: Sequelize.STRING,
             used: Sequelize.BOOLEAN
         },
