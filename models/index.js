cbb62084a003ebbc467c38b5ed40f45d0df7cfe2
sexozavr <daniildiveev@mail.ru>
2023-04-18 22:37:06 +0300

    [MODIFY] add invite link model to index.js

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const UserModel = require('./user');
 const PollModel = require('./poll');
 const ReactionTestModel = require('./reactionTest')
 const ComplexReactionTestModel = require('./complexReactionTest')
+const InviteLinkModel = require('./inviteLink')
 
 const sequelize = new Sequelize('opd_2_sem', 'postgres', 'N29EXFdw', {
     dialect: 'postgres',
@@ -13,11 +14,13 @@ const User = UserModel(sequelize);
 const Poll = PollModel(sequelize);
 const ReactionTest = ReactionTestModel(sequelize);
 const ComplexReactionTest = ComplexReactionTestModel(sequelize);
+const InviteLink = InviteLinkModel(sequelize);
 
 module.exports = {
     sequelize,
     User,
     Poll,
     ReactionTest,
-    ComplexReactionTest
+    ComplexReactionTest,
+    InviteLink
 };
