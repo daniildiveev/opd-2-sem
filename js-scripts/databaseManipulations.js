21b3a25111bb49979624104ccc0acf55be52b684
sexozavr <daniildiveev@mail.ru>
2023-04-18 22:41:13 +0300

    [ADD] implement functions for comfortable information retrieval from database

---
+++ js-scripts/databaseManipulations.js
@@ -0,0 +1,94 @@
+const {Poll, ReactionTest, ComplexReactionTest, User} = require('../models')
+
+async function filterTest(type, username, testId, testType){
+    let userId = null
+
+    if (username){
+        const user = await User.findOne({
+            where: {
+                login: username
+            }
+        })
+
+        userId = user.id
+    }
+
+
+    if (type === "characteristicsTest"){
+        if (testId){
+            return await Poll.findByPk(testId)
+        }
+
+        if (userId){
+            return await Poll.findAll(
+                {
+                    where: {
+                        user: userId
+                    }
+                }
+            )
+        } else {
+            return await Poll.findAll()
+        }
+
+    } else if (type === "reactionTest"){
+        if (testId){
+            return await ReactionTest.findByPk(testId)
+        }
+
+        let filters = {}
+
+        if(testType){
+            filters["type"] = testType
+        }
+
+        if (userId){
+            filters["user"] = userId
+        }
+
+        if (filters){
+            return await ReactionTest.findAll({
+                where : filters
+            })
+        } else {
+            return await ReactionTest.findAll()
+        }
+    } else {
+        if (testId){
+            return await ComplexReactionTest.findByPk(testId)
+        }
+
+        let filters = {}
+
+        if (userId){
+            filters["user"] = userId
+        }
+
+        if(testType){
+            filters["type"] = testType
+        }
+
+        if (filters){
+            return await ComplexReactionTest.findAll({
+                where : filters
+            })
+        } else {
+            return await ComplexReactionTest.findAll()
+        }
+    }
+}
+
+async function getUsers(){
+    const users = await User.findAll()
+    let logins = []
+
+    if(users) {
+        users.forEach(user => {
+            logins.push(user.login)
+        })
+    }
+
+    return logins
+}
+
+module.exports = {filterTest, getUsers}
